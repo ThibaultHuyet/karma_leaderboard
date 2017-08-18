7d90598ed7010bde496487829e3cd0efef43a48b
Thibault Huyet <thibault.huyet@gmail.com>
2017-08-18 15:00:04 +0100

    Added comments

--- reddit_rankings.py
+++ reddit_rankings.py
@@ -1,4 +1,7 @@
 import praw
+
+# Settings file is created by the user
+# Due to sensitive data, this is ignored by github
 from settings import CI, CS, PW, UA, UN
 
 reddit= praw.Reddit(client_id = CI,
@@ -7,6 +10,12 @@ reddit= praw.Reddit(client_id = CI,
                     user_agent = UA,
                     username = UN
                     )
+
+"""
+Submission id's is used to assure no duplicates
+Submission score is the karma associated with the post
+Submission subreddit is the subreddit the score goes towards
+"""
 
 for submission in reddit.subreddit('all').hot(limit = 25):
     print(str(submission.subreddit) + ":\t" +
