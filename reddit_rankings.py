63f5b3145d253702075eff2e0c063cc392b80060
Thibault Huyet <thibault.huyet@gmail.com>
2017-08-18 13:28:54 +0100

    Added main python file

---
+++ reddit_rankings.py
@@ -0,0 +1,14 @@
+import praw
+from settings import CI, CS, PW, UA, UN
+
+reddit= praw.Reddit(client_id = CI,
+                    client_secret = CS,
+                    password = PW,
+                    user_agent = UA,
+                    username = UN
+                    )
+
+for submission in reddit.subreddit('all').hot(limit = 25):
+    print(str(submission.subreddit) + ":\t" +
+          str(submission.score) + ":\t" +
+          str(submission.url))
