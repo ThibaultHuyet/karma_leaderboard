e6319c7e9829fe8f2e07dddaef63eeeca5d7dc4d
Thibault Huyet <thibault.huyet@gmail.com>
2017-08-21 10:38:23 +0100

    Added mongodb

--- reddit_rankings.py
+++ reddit_rankings.py
@@ -1,4 +1,6 @@
 import praw
+from pymongo import MongoClient
+import datetime
 
 # Settings file is created by the user
 # Due to sensitive data, this is ignored by github
@@ -11,13 +13,19 @@ reddit= praw.Reddit(client_id = CI,
                     username = UN
                     )
 
-"""
-Submission id's is used to assure no duplicates
-Submission score is the karma associated with the post
-Submission subreddit is the subreddit the score goes towards
-"""
+def add_to_collection(client, db, posts):
+	for submission in reddit.subreddit('all').hot(limit = 50):
+		post = {
+				"subreddit" : str(submission.subreddit),
+				"link_score" : int(submission.score),
+				"post_id" : str(submission.id),
+				"time" : datetime.datetime.utcnow()
+				}
+		post_id = posts.insert_one(post).inserted_id
 
-for submission in reddit.subreddit('all').hot(limit = 25):
-    print(str(submission.subreddit) + ":\t" +
-          str(submission.score) + ":\t" +
-          str(submission.id))
+
+client = MongoClient()
+db = client.REDDIT_RANKINGS
+posts = db.karma_leaderboard
+
+add_to_collection(client, db, posts)
