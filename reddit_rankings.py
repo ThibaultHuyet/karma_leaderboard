d5bfd7885741838fa43412174fc39ff89194aeec
Thibault Huyet <thibault.huyet@gmail.com>
2017-08-21 15:11:11 +0100

    redid struture of MongoDB database

--- reddit_rankings.py
+++ reddit_rankings.py
@@ -1,7 +1,7 @@
 import praw
 from pymongo import MongoClient
 import datetime
-
+import pprint
 # Settings file is created by the user
 # Due to sensitive data, this is ignored by github
 from settings import CI, CS, PW, UA, UN
@@ -13,39 +13,87 @@ reddit= praw.Reddit(client_id = CI,
                     username = UN
                     )
 
-def add_to_collection(client, db, posts):
+def add_to_collection(client, db, posts, checked, subs):
 
 	"""
 	This function adds posts to a database
 	"""
 
-	for submission in reddit.subreddit('all').hot(limit = 50):
-		post = {
-				"subreddit" : str(submission.subreddit),
-				"link_score" : int(submission.score),
-				"post_id" : str(submission.id),
-				"time" : datetime.datetime.utcnow()
-				}
-		post_id = posts.insert_one(post).inserted_id
+	for submission in reddit.subreddit('all').hot(limit = 500):
+		"""
+		I need to add a conditional that prevents adding repeat posts
+		However, under certain circumstances, these repeats should be updated
+		"""
+		if str(submission.subreddit) not in subs:
+			post = {
+					"_id" : str(submission.subreddit),
+					"post": {
+							"link_score" : int(submission.score),
+							"post_id" : str(submission.id),
+							"time" : datetime.datetime.utcnow()
+							},
+					"score" : int(submission.score)
+					}
+
+			result = posts.insert_one(post).inserted_id
+
+			subs.append(str(submission.subreddit))
+			checked[str(submission.id)] = int(submission.score) 
+
+		elif str(submission.subreddit) in subs and str(submission.id) not in checked:
+			"""
+			Subreddit is in the list but submission is not
+			This is where the update logic should occur
+			It is needed to update the posts in the subreddit area
+			"""
+			posts.update_one({"_id" : str(submission.subreddit)},
+							{'$set':{
+									"post.post_id" : str(submission.id),
+									"post.link_score" : int(submission.score),
+									"post.time" : datetime.datetime.utcnow()
+									}
+							},
+							upsert = False)
+			
+			posts.update_one({"_id" : str(submission.subreddit)},
+							{'$inc' : {'score' : +int(submission.score), "score":1}}
+							)
+			
+			checked[str(submission.id)] = int(submission.score)
+
+		else:
+			"""
+			The only situation left would be that the subreddit and post are in the database
+			Thus, I do not care
+			"""
+			pass
+
+	return checked, subs
 
 def check_database(client, db, posts):
 
 	"""
 	This function returns a dictionary of every post in the database
 	The dictionary is of the submission_id and the link_score
+	The list is a list of all the subreddits
 	"""
 
 	d = {}
+	l = []
 
-	for post in posts.find({}, {"post_id":1, "_id":0, "link_score":1}):
-		d[post["post_id"]] = post["link_score"]
 
-	return d
+	for post in posts.find({}, {"_id":1, "post.post_id":1, "post.link_score":1}):
+		d[post["post"]["post_id"]] = post["post"]["link_score"]
+		l.append(post["_id"])
+
+	return d, l
 
 client = MongoClient()
 db = client.REDDIT_RANKINGS
 posts = db.karma_leaderboard
 
-# add_to_collection(client, db, posts)
+checked = {}
+subs = []
 
-checked = check_database(client, db, posts)
+checked, subs = check_database(client, db, posts)
+checked, subs = add_to_collection(client, db, posts, checked, subs)
